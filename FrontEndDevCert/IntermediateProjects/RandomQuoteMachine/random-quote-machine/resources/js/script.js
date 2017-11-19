c1097fb40dbc6c063fc9191b163b82d1876a52d4
Robin Goyal <goyal.rob@gmail.com>
2017-11-19 15:22:30 -0500

    Twitter share button for quote added

--- FrontEndDevCert/IntermediateProjects/RandomQuoteMachine/random-quote-machine/resources/js/script.js
+++ FrontEndDevCert/IntermediateProjects/RandomQuoteMachine/random-quote-machine/resources/js/script.js
@@ -3,9 +3,11 @@ let UpdateQuote = function() {
     // AJAX request for random quote
     $.getJSON("https://talaikis.com/api/quotes/random/", function(json) {
 
+        let quote = json['quote'];
+        let author = json['author']
+
         // Fade out content
         $("#quote span, #author, i").delay(200).fadeOut(1200, function() {
-
             // Replace quote and author with returned data
             $('#quote span').text(json['quote']);
             $('#author').text(json['author']);
@@ -13,6 +15,11 @@ let UpdateQuote = function() {
             // Fade content in
             $("#quote span, #author, i").fadeIn(900);
         });
+
+        // Append quote and author text to twitter button
+        let twitter_href = $("#twitter-share-button").attr('href');
+        href_text = encodeURIComponent(quote) + encodeURIComponent(' - ') + encodeURIComponent(author);
+        $("#twitter-share-button").attr("href", twitter_href + href_text);
     });
 }
 
