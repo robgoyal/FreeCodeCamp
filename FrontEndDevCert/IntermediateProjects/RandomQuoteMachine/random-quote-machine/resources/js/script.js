c9c909d7ac990f9a8ecf0427308f74f4312d2913
Robin Goyal <goyal.rob@gmail.com>
2017-11-19 16:33:20 -0500

    Random color change for content

--- FrontEndDevCert/IntermediateProjects/RandomQuoteMachine/random-quote-machine/resources/js/script.js
+++ FrontEndDevCert/IntermediateProjects/RandomQuoteMachine/random-quote-machine/resources/js/script.js
@@ -6,8 +6,29 @@ let UpdateQuote = function() {
         let quote = json['quote'];
         let author = json['author']
 
+        let random_color = RandomColor();
+            $("body").css({
+                'transition': 'background-color 2s ease-in',
+                'background-color': random_color
+            });
+
+            $("#twitter-share-button, #btn-random-quote, #quote span, #author, i").css({
+                'transition': 'color 2s ease-in',
+                'color': random_color
+            })
         // Fade out content
         $("#quote span, #author, i").delay(200).fadeOut(1200, function() {
+
+            // let random_color = RandomColor();
+            // $("body").css({
+            //     'transition': 'background-color 0.9s ease-in',
+            //     'background-color': random_color
+            // });
+
+
+
+            //$("#quote span, #author, i, ")
+
             // Replace quote and author with returned data
             $('#quote span').text(json['quote']);
             $('#author').text(json['author']);
@@ -21,6 +42,18 @@ let UpdateQuote = function() {
         href_text = encodeURIComponent(quote) + encodeURIComponent(' - ') + encodeURIComponent(author);
         $("#twitter-share-button").attr("href", twitter_href + href_text);
     });
+}
+
+let RandomColor = function() {
+    let color = ['#'];
+    let values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                  'A', 'B', 'C', 'D', 'E', 'F'];
+
+    for (let i = 0; i < 6; i++) {
+        color.push(values[Math.floor(Math.random() * values.length)]);
+    }
+
+    return color.join("");
 }
 
 $(document).ready(function() {
