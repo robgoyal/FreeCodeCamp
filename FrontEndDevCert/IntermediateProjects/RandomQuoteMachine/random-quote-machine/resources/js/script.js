258e8b9a7740cb3103039924cbe01c9c364edb80
Robin Goyal <goyal.rob@gmail.com>
2017-11-19 16:50:08 -0500

    Cleanup

--- FrontEndDevCert/IntermediateProjects/RandomQuoteMachine/random-quote-machine/resources/js/script.js
+++ FrontEndDevCert/IntermediateProjects/RandomQuoteMachine/random-quote-machine/resources/js/script.js
@@ -1,33 +1,33 @@
+/* Name: script.js
+   Author: Robin Goyal
+   Last-Modified: November 19, 2017
+   Purpose: Script to handle dynamic content for random quote project
+*/
+
 let UpdateQuote = function() {
 
     // AJAX request for random quote
     $.getJSON("https://talaikis.com/api/quotes/random/", function(json) {
 
+        // Retrieve quote and author data
         let quote = json['quote'];
         let author = json['author']
 
         let random_color = RandomColor();
-            $("body").css({
-                'transition': 'background-color 2s ease-in',
-                'background-color': random_color
-            });
+        // Change background color
+        $("body").css({
+            'transition': 'background-color 2s ease-in',
+            'background-color': random_color
+        });
 
-            $("#twitter-share-button, #btn-random-quote, #quote span, #author, i").css({
-                'transition': 'color 2s ease-in',
-                'color': random_color
-            })
+        // Change content color
+        $("#twitter-share-button, #btn-random-quote, #quote span, #author, i").css({
+            'transition': 'color 2s ease-in',
+            'color': random_color
+        })
+
         // Fade out content
         $("#quote span, #author, i").delay(200).fadeOut(1200, function() {
-
-            // let random_color = RandomColor();
-            // $("body").css({
-            //     'transition': 'background-color 0.9s ease-in',
-            //     'background-color': random_color
-            // });
-
-
-
-            //$("#quote span, #author, i, ")
 
             // Replace quote and author with returned data
             $('#quote span').text(json['quote']);
@@ -35,6 +35,7 @@ let UpdateQuote = function() {
 
             // Fade content in
             $("#quote span, #author, i").fadeIn(900);
+
         });
 
         // Append quote and author text to twitter button
@@ -45,14 +46,18 @@ let UpdateQuote = function() {
 }
 
 let RandomColor = function() {
+
+    // Initialize hex color
     let color = ['#'];
     let values = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
                   'A', 'B', 'C', 'D', 'E', 'F'];
 
+    // Push random hex value
     for (let i = 0; i < 6; i++) {
         color.push(values[Math.floor(Math.random() * values.length)]);
     }
 
+    // Create string for hex color
     return color.join("");
 }
 
