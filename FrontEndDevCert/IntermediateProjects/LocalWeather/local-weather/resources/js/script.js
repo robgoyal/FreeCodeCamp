bf17d462d8640045c35d7493793e97c9939e93f8
Robin Goyal <goyal.rob@gmail.com>
2017-11-27 20:50:16 -0500

    Minor css changes, added button to switch temperature scales, completed functionality

--- FrontEndDevCert/IntermediateProjects/LocalWeather/local-weather/resources/js/script.js
+++ FrontEndDevCert/IntermediateProjects/LocalWeather/local-weather/resources/js/script.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
-
     // Check if browser supports geolocation
     if (navigator.geolocation) {
 
         // Get current position
         navigator.geolocation.getCurrentPosition(function(position) {
+
+            // Access latitude and longitude from returned position data
             let latitude = position.coords.latitude;
             let longitude = position.coords.longitude;
+
+            // Declare global variables for event handler to access
+            let temperature;
+            let is_celcius = true;
 
             // Initialize request url for weather
             let url = `https://fcc-weather-api.glitch.me/api/current?lat=${latitude}&lon=${longitude}`;
@@ -17,21 +22,46 @@ $(document).ready(function() {
                 // Retrieve weather information from returned data
                 let country_code = data['sys']['country'];
                 let region_name = data['name'];
-                let temperature_celcius = data['main']['temp'];
+                temperature = data['main']['temp'];
                 let description = data['weather'][0]['description'];
                 let icon_src = data['weather'][0]['icon'];
 
+                // Set information
                 $("#location").text(region_name + ', ' + country_code);
-                $("#temperature").text(temperature_celcius + " C");
                 $("#description").text(description);
                 $("#weather-icon").attr("src", icon_src);
+
+                // Create HTML for temperature and button for temperature scale
+                $("#temperature").html(Math.trunc(temperature) + '<button id="scale">\u00b0C</button>');
+            });
+
+            // Onclick function to handle switching temperature scale
+            $("body").on('click', '#scale', function() {
+
+                // Convert celcius to fahrenheit
+                if (is_celcius) {
+                    temperature = temperature * 1.8 + 32;
+                    $("#temperature").html(Math.trunc(temperature) + '<button id="scale">\u00b0F</button>');
+                }
+
+                // Convert fahrenheit to celcius
+                else {
+                    temperature = (temperature - 32) / 1.8;
+                    $("#temperature").html(Math.trunc(temperature) + '<button id="scale">\u00b0C</button>');
+                }
+
+                // Negate boolean value
+                is_celcius = !(is_celcius);
             });
         });
     }
+
+
 
     // Browser does not support geolocation information
     else {
         console.log("Geolocation information not available!");
     }
+
 });
 
