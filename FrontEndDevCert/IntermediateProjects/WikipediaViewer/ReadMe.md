3e5173530948077fc0c41bc97446e3efc65c81c2
Robin Goyal <goyal.rob@gmail.com>
2018-01-16 16:25:55 -0500

    Initial commit for Wikipedia Viewer project

---
+++ FrontEndDevCert/IntermediateProjects/WikipediaViewer/ReadMe.md
@@ -0,0 +1,5 @@
+## WikipediaViewer
+
+### Purpose
+
+A single page web application which displays results from Wikipedia's API after a search is complete or redirects you to Wikipedia for a random article after clicking a button.
---
+++ FrontEndDevCert/IntermediateProjects/WikipediaViewer/wikipedia-viewer/resources/js/script.js
@@ -0,0 +1,16 @@
+/* Name: script.js
+   Author: Robin Goyal
+   Last-Modified: January 16, 2018
+   Purpose: Server-side functionality for requesting Wikipedia
+            articles through the API
+*/
+
+$(document).ready(function() {
+
+
+});
+
+
+let search_results = function() {
+
+}
