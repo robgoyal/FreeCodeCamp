63d573a923464c03310923d9fe02b12027c599ee
Robin Goyal <goyal.rob@gmail.com>
2018-01-19 12:49:39 -0500

    Added ability to enter or click button for form submission

--- FrontEndDevCert/IntermediateProjects/WikipediaViewer/wikipedia-viewer/resources/js/script.js
+++ FrontEndDevCert/IntermediateProjects/WikipediaViewer/wikipedia-viewer/resources/js/script.js
@@ -1,22 +1,41 @@
 /* Name: script.js
    Author: Robin Goyal
-   Last-Modified: January 18, 2018
+   Last-Modified: January 19, 2018
    Purpose: Client-side functionality for requesting Wikipedia
             articles through the API
 */
 
 $(document).ready(function() {
-  $.ajax({
-    url: "https://en.wikipedia.org/w/api.php",
-    data: {
-      action: 'query',
-      list: 'search',
-      srsearch: "Albert Einstein",
-      format: "json"
-    },
-    dataType: 'jsonp',
-    success: function(x) {
-      console.log(x);
+
+  /*
+  NOTE: creating a link for each page can be done by
+  adding curid=pageid to https://en.wikipedia.org/
+  */
+
+  document.getElementById('query').addEventListener('submit', function(evt) {
+    evt.preventDefault();
+    console.log(evt.target[0].value);
+    $.ajax({
+      url: "https://en.wikipedia.org/w/api.php",
+      data: {
+        action: 'query',
+        list: 'search',
+        srsearch: evt.target[0].value,
+        format: "json"
+      },
+      dataType: 'jsonp',
+      success: function(x) {
+        console.log(x);
+      }
+    });
+
+
+  })
+
+  $('#text').keyup(function(event) {
+    if (event.keyCode === 13) {
+      $("#enter").submit();
     }
   })
+
 });
